573a33b0c8ec1c07f708572fc05d40c79d4c3cfe
Gordy <gordy2007@hotmail.co.uk>
2025-01-08 14:45:22 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+dnf install -y tmux 
+
+# this would install a package from rpmfusion
+# rpm-ostree install vlc
+
+#### Example for enabling a System Unit File
+
+systemctl enable podman.socket
